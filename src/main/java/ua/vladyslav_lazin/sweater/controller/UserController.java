cc063b23dc8d63b04800534617e3121cf65a1fa7
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2022-12-16 11:15:05 +0200

    Add user profile, it haven't worked yet

--- src/main/java/ua/vladyslav_lazin/sweater/controller/UserController.java
+++ src/main/java/ua/vladyslav_lazin/sweater/controller/UserController.java
@@ -1,54 +1,64 @@
 package ua.vladyslav_lazin.sweater.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ua.vladyslav_lazin.sweater.entity.Role;
 import ua.vladyslav_lazin.sweater.entity.User;
-import ua.vladyslav_lazin.sweater.repository.UserRepository;
+import ua.vladyslav_lazin.sweater.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAnyAuthority('ADMIN')")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
         return "userEdit";
     }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
-            @RequestParam("userId")User user) {
-        user.setUsername(username);
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-        user.getRoles().clear();
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepository.save(user);
+            @RequestParam("userId") User user) {
+        userService.saveUser(user, username, form);
         return "redirect:user";
     }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+        @AuthenticationPrincipal User user,
+        @RequestParam String password,
+        @RequestParam String email) {
+            userService.updateProfile(user, password, email);
+            return "redirect:/user/profile";
+        }
 }
--- src/main/java/ua/vladyslav_lazin/sweater/service/UserService.java
+++ src/main/java/ua/vladyslav_lazin/sweater/service/UserService.java
@@ -9,8 +9,13 @@ import ua.vladyslav_lazin.sweater.entity.Role;
 import ua.vladyslav_lazin.sweater.entity.User;
 import ua.vladyslav_lazin.sweater.repository.UserRepository;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -26,6 +31,7 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username);
     }
+
     public boolean addUser(User user) {
         User userFromDb = userRepository.findByUsername(user.getUsername());
         boolean userFlag;
@@ -36,18 +42,12 @@ public class UserService implements UserDetailsService {
             user.setRoles(Collections.singleton(Role.USER));
             user.setActivationCode(UUID.randomUUID().toString());
             userRepository.save(user);
-            if (StringUtils.hasLength(user.getEmail())) {
-                String message = String.format(
-                        "Hello, %s! \n" + "Welcome to Swearter. Please, visit the next link: http://localhost:8081/activate/%s",
-                        user.getUsername(),
-                        user.getActivationCode()
-                );
-                mailSender.sendMail(user.getEmail(), "Activation code", message);
-            }
+            sendMessage(user);
             userFlag = true;
         }
         return userFlag;
     }
+
     public boolean activateUser(String code) {
         User user = userRepository.findByActivationCode(code);
         boolean userFlag = true;
@@ -58,5 +58,57 @@ public class UserService implements UserDetailsService {
             userRepository.save(user);
         }
         return userFlag;
+    }
+
+    private void sendMessage(User user) {
+        if (StringUtils.hasLength(user.getEmail())) {
+            String message = String.format(
+                    "Hello, %s! \n"
+                            + "Welcome to Swearter. Please, visit the next link: http://localhost:8081/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode());
+            mailSender.sendMail(user.getEmail(), "Activation code", message);
+        }
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+        if (isEmailChanged) {
+            user.setEmail(email);
+
+            if (StringUtils.hasLength(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (StringUtils.hasLength(password)) {
+            user.setPassword(password);
+        }
+        userRepository.save(user);
+
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
