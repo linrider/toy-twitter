83ef67ce847c0ae359c3704290d016dedccc0732
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2023-03-07 09:16:35 +0200

    pom changes

--- src/main/java/ua/vladyslav_lazin/sweater/entity/User.java
+++ src/main/java/ua/vladyslav_lazin/sweater/entity/User.java
@@ -34,6 +34,9 @@ public class User implements UserDetails {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    
+    
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
     }
